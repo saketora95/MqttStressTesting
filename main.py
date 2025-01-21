aa0f77199bcf5ac976b82f1f5009ea3eb8ed4534
kai-smms <chengkai.hong@smms.com.tw>
2025-01-21 12:06:12 +0800

    Update config format

--- main.py
+++ main.py
@@ -5,3 +5,25 @@ import config_reader
 
 program_config = config_reader.load_config('config.txt')
 print(program_config)
+
+# ----- ----- ----- ----- -----
+
+def on_connect(client, userdata, flags, reason_code, properties):
+    print(f"Connected with result code {reason_code}")
+    # client.subscribe("$SYS/#")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+
+mqttc.connect("mqtt.eclipseprojects.io", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+mqttc.loop_forever()
